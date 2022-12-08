30fa7c4aa947501c98ed5c31f2a1bc2bff25df1b
Mohammed Mostafa <mohmiim@gmail.com>
2022-12-08 13:36:16 -0500

    stock profit

--- best_time_to_buy/src/main.rs
+++ best_time_to_buy/src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
-    println!("Hello, world!");
+    let profit = max_profit(vec![7,1,5,3,6,4]);
+    println!("{profit}");
 }
+
+pub fn max_profit(prices: Vec<i32>) -> i32 {
+    let mut max_profit = 0;
+    let mut min_index = 0;
+    for index in 1..prices.len() {
+        if prices[index] < prices[min_index] {
+            min_index = index;
+        } else {
+            let profit = prices[index] - prices[min_index];
+            if profit > max_profit {
+                max_profit = profit
+            }
+        }
+    }
+    return max_profit;
+}
+
+
+/*
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0 ;
+        int min  = prices[0];
+        for (int index = 1; index < prices.length ; index++){
+            if (prices[index] < min ){
+                min = prices[index];
+            }else {
+                int localProfit = prices[index] - min;
+                if (localProfit > profit){
+                    profit = localProfit;
+                }
+            }
+        }
+        return profit;
+    }
+}
+
+
+*/
